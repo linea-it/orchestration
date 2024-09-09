4a111e0538d356d5dbe7b4de5982a7229b559de7
Cristiano Singulani <crisingulani@gmail.com>
2024-09-09 20:48:29 +0000

    Small improvements  to test enviroment

--- backend/orchestration/settings.py
+++ backend/orchestration/settings.py
@@ -21,13 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space
 # between each. For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1 [::1]").split(
-    " "
-)
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "*").split(" ")
 
 # https://docs.djangoproject.com/en/4.0/releases/4.0/#csrf-trusted-origins-changes
 CSRF_TRUSTED_ORIGINS = os.getenv(
-    "DJANGO_CSRF_TRUSTED_ORIGINS", "http://localhost http://127.0.0.1"
+    "DJANGO_CSRF_TRUSTED_ORIGINS", "http://*"
 ).split(" ")
 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#csrf-cookie-name
@@ -36,7 +34,7 @@ CSRF_COOKIE_NAME = "orchestration.csrftoken"
 
 # rabbitmq
 AMQP_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
-AMQP_PORT = os.getenv("RABBITMQ_PORT","5672")
+AMQP_PORT = os.getenv("RABBITMQ_PORT", "5672")
 AMQP_USER = os.getenv("RABBITMQ_DEFAULT_USER", "orcadmin")
 AMQP_PASS = os.getenv("RABBITMQ_DEFAULT_PASS", "adminorc")
 AMQP_VHOST = os.getenv("RABBITMQ_DEFAULT_VHOST", "/")
