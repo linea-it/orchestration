dff2fbc919a875f55a8ca1c5dd77d356d16bc3b2
Cristiano Singulani <crisingulani@gmail.com>
2026-03-12 14:05:14 -0300

    fixed small bug in MAMBA_ROOT_PREFIX

--- backend/sh/env.sh
+++ backend/sh/env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash --login
 
 export MICROMAMBA_BIN="${MICROMAMBA_BIN:-micromamba}"
-export MAMBA_ROOT_PREFIX="${MAMBA_ROOT_PREFIX:-/opt/micromamba}"
+export ORCHESTRATION_ENV_PREFIX="${ORCHESTRATION_ENV_PREFIX:-/opt/micromamba/envs/orchestration}"
+export MAMBA_ROOT_PREFIX="${MAMBA_ROOT_PREFIX:-${BASE_DIR}/.micromamba}"
 export ORCHESTRATION_ENV_NAME="${ORCHESTRATION_ENV_NAME:-orchestration}"
-export PATH="${MAMBA_ROOT_PREFIX}/envs/${ORCHESTRATION_ENV_NAME}/bin:${PATH}"
+export PATH="${ORCHESTRATION_ENV_PREFIX}/bin:${PATH}"
